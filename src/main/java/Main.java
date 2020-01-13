4e15f019747bea5d99b7187d5b82f80a15b5f07c
Nicklas Utgaard <nicklas.utgaard@nav.no>
2020-01-13 08:25:26 +0100

    [KAIZEN-0] fjerner ubrukt avhengighet til isso-rp-user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import no.nav.apiapp.ApiApp;
-import no.nav.brukerdialog.security.Constants;
 import no.nav.brukerdialog.tools.SecurityConstants;
 import no.nav.common.nais.utils.NaisUtils;
 import no.nav.sbl.config.ApplicationConfig;
@@ -17,13 +16,10 @@ public class Main {
 
     private static void setupVault() {
         NaisUtils.Credentials serviceUser = NaisUtils.getCredentials("service_user");
+
         EnvironmentUtils.setProperty(EnvironmentUtils.resolveSrvUserPropertyName(), serviceUser.username, PUBLIC);
         EnvironmentUtils.setProperty(EnvironmentUtils.resolverSrvPasswordPropertyName(), serviceUser.password, SECRET);
         EnvironmentUtils.setProperty(SecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username, PUBLIC);
         EnvironmentUtils.setProperty(SecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password, SECRET);
-
-        NaisUtils.Credentials issoRPUser = NaisUtils.getCredentials("isso-rp-user");
-        EnvironmentUtils.setProperty(Constants.ISSO_RP_USER_USERNAME_PROPERTY_NAME, issoRPUser.username, PUBLIC);
-        EnvironmentUtils.setProperty(Constants.ISSO_RP_USER_PASSWORD_PROPERTY_NAME, issoRPUser.password, SECRET);
     }
 }
