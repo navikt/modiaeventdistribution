70cf3d22d1e1c13a91f42d1337a1d36400431980
Nicklas Utgaard <nutgaard@gmail.com>
2020-08-14 10:52:44 +0200

    [OPP-1211] tydliggjøre valg av vault-source

--- src/test/kotlin/no/nav/modiaeventdistribution/MainTest.kt
+++ src/test/kotlin/no/nav/modiaeventdistribution/MainTest.kt
@@ -1,20 +1,30 @@
 package no.nav.modiaeventdistribution
 
+import dev.nohus.autokonfig.AutoKonfig
+import dev.nohus.autokonfig.withMap
 import no.nav.common.nais.NaisYamlUtils
 
+// .vault.properties skal inneholder noe alà (hentes fra vault om nødvendig);
+// service_user_username=<srv username>
+// service_user_password=<srv password>
+fun AutoKonfig.withVaultProps(brukVaultFil: Boolean) = apply {
+    if (brukVaultFil) {
+        withProperties(".vault.properties")
+    } else {
+        withMap(mapOf(
+                "service_user_username" to "N/A",
+                "service_user_password" to "N/A"
+        ))
+    }
+}
 
 fun main() {
     NaisYamlUtils
             .getTemplatedConfig(".nais/qa-template.yml", mapOf("namespace" to "q0"))
             .also(NaisYamlUtils::loadFromYaml)
     val config = ConfigLoader{
-        withProperties(".vault.properties")
+        withVaultProps(brukVaultFil = false)
         withProperty("port", "8081")
-//        withMap(mapOf(
-//                "service_user_username" to "vault",
-//                "service_user_password" to "vault",
-//                "port" to "8081"
-//        ))
     }.load()
 
     startApplication(config)
