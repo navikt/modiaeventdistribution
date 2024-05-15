e29ccebcea8df41e0e35ff6609916b3eb4eb8176
Ludvig Hozman <ludvig123@gmail.com>
2024-05-15 10:42:04 +0200

    Deploy q2 til dev-gcp
    Fjerner ubrukt service_user hentet fra vault & konfigurerbar basepath
    for appen.

--- src/main/kotlin/no/nav/modiaeventdistribution/Config.kt
+++ src/main/kotlin/no/nav/modiaeventdistribution/Config.kt
@@ -2,37 +2,10 @@ package no.nav.modiaeventdistribution
 
 import dev.nohus.autokonfig.*
 import dev.nohus.autokonfig.types.IntSetting
+import dev.nohus.autokonfig.types.OptionalStringSetting
 import dev.nohus.autokonfig.types.StringSetting
-import no.nav.common.utils.NaisUtils
-import java.io.IOException
-import java.util.*
 import java.util.Collections.singletonMap
 
-fun AutoKonfig.withVaultCredentials(secretName: String) = apply {
-    try {
-        val credentials = NaisUtils.getCredentials(secretName)
-        withMap(
-            mapOf(
-                "${secretName}_username" to credentials.username,
-                "${secretName}_password" to credentials.password
-            ),
-            "vault secret ($secretName)"
-        )
-    } catch (e: Throwable) {}
-}
-
-fun AutoKonfig.withProperties(file: String) = apply {
-    val stream = Config::class.java.classLoader.getResourceAsStream(file)
-        ?: throw RuntimeException("Resource file ($file) not found")
-
-    val properties = try {
-        Properties().apply { load(stream) }
-    } catch (e: IOException) {
-        throw RuntimeException("Could not read file ($file)")
-    }
-
-    withProperties(properties, "properties file ($file)")
-}
 
 fun AutoKonfig.withProperty(key: String, value: String) = apply {
     withMap(singletonMap(key, value))
@@ -42,6 +15,7 @@ class Config internal constructor() {
     val appName by StringSetting()
     val appEnvironment by StringSetting()
     val appVersion by StringSetting()
+    val basePath by OptionalStringSetting()
     val port by IntSetting(8080)
 }
 
@@ -51,7 +25,6 @@ class ConfigLoader(block: AutoKonfig.() -> Unit = {}) {
             .clear()
             .withSystemProperties()
             .withEnvironmentVariables()
-            .withVaultCredentials("service_user")
             .apply(block)
     }
 
--- src/test/kotlin/no/nav/modiaeventdistribution/MainTest.kt
+++ src/test/kotlin/no/nav/modiaeventdistribution/MainTest.kt
@@ -4,28 +4,12 @@ import dev.nohus.autokonfig.AutoKonfig
 import dev.nohus.autokonfig.withMap
 import no.nav.common.nais.NaisYamlUtils
 
-// .vault.properties skal inneholder noe alà (hentes fra vault om nødvendig);
-// service_user_username=<srv username>
-// service_user_password=<srv password>
-fun AutoKonfig.withVaultProps(brukVaultFil: Boolean) = apply {
-    if (brukVaultFil) {
-        withProperties(".vault.properties")
-    } else {
-        withMap(
-            mapOf(
-                "service_user_username" to "N/A",
-                "service_user_password" to "N/A"
-            )
-        )
-    }
-}
 
 fun main() {
     NaisYamlUtils
         .getTemplatedConfig(".nais/qa-template.yml", mapOf("namespace" to "q0"))
         .also(NaisYamlUtils::loadFromYaml)
     val config = ConfigLoader {
-        withVaultProps(brukVaultFil = false)
         withProperty("port", "8081")
     }.load()
 
