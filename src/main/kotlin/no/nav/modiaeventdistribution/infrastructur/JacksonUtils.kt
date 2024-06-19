e5777ed60e0e5272c4ce20b1ca499348c314b742
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-06-19 14:12:51 +0200

    Tillat ukjente properties i JSON

--- src/main/kotlin/no/nav/modiaeventdistribution/infrastructur/JacksonUtils.kt
+++ src/main/kotlin/no/nav/modiaeventdistribution/infrastructur/JacksonUtils.kt
@@ -3,28 +3,25 @@ package no.nav.modiaeventdistribution.infrastructur
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.core.util.DefaultIndenter
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 object JacksonUtils {
-    val objectMapper = jacksonObjectMapper()
-        .apply {
-            configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-            setDefaultPrettyPrinter(
-                DefaultPrettyPrinter().apply {
-                    indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
-                    indentObjectsWith(DefaultIndenter("  ", "\n"))
-                }
-            )
-            deactivateDefaultTyping()
-            enable(SerializationFeature.INDENT_OUTPUT)
-        }
+    val objectMapper =
+        jacksonObjectMapper()
+            .apply {
+                configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                setDefaultPrettyPrinter(
+                    DefaultPrettyPrinter().apply {
+                        indentArraysWith(DefaultPrettyPrinter.FixedSpaceIndenter.instance)
+                        indentObjectsWith(DefaultIndenter("  ", "\n"))
+                    },
+                )
+                deactivateDefaultTyping()
+                enable(SerializationFeature.INDENT_OUTPUT)
+                configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            }
 }
 
-inline fun <reified T> T.toJson(): String {
-    return JacksonUtils.objectMapper.writeValueAsString(this)
-}
-
-inline fun <reified T> String.fromJson(): T {
-    return JacksonUtils.objectMapper.readValue(this, object : TypeReference<T>() {})
-}
+inline fun <reified T> String.fromJson(): T = JacksonUtils.objectMapper.readValue(this, object : TypeReference<T>() {})
